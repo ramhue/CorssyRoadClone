47984b62a27fe73a0523c6cb2f7b965bdbbf29c9
ramhue <kbframiro@yahoo.com>
2018-12-24 00:46:14 -0800

    hi

---
+++ Source/crossyroadsF/crossyroadsFGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "crossyroadsFGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CROSSYROADSF_API AcrossyroadsFGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
